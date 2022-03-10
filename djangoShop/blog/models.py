d426aad247810f7aaae54c6fdadcb81f424f95b8
Alex_djangp_projects <aleksone_01@mail.ru>
2022-03-10 21:15:36 +0300

    Create Model Blog

--- djangoShop/blog/models.py
+++ djangoShop/blog/models.py
@@ -3,9 +3,30 @@ from django.db import models
 from taggit.managers import TaggableManager
 
 # Create your models here.
+
+class Category(models.model):
+    name = models.CharField(max_length=60)
+
 class Blog(models.Model):
     title = models.CharField(max_length=60)
     image = models.ImageField()
     description = models.TextField()
     date = models.DateField(auto_now=False, auto_now_add=True)
     tags = TaggableManager()
+    categories = models.ManyToManyField(Category)
+
+class Comment(models.Model):
+    post = models.ForeignKey(Blog, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created',)
+
+    def __str__(self):
+        return 'Comment by {} on {}'.format(self.name, self.post)
+
